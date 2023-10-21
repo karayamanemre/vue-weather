568acae3101f016a7568b57b895a22256a2430f2
Emre Karayaman <karayaman.emre@outlook.com>
2023-10-21 15:50:55 +0300

    Create AsyncCityView.vue

---
+++ vue-weather/src/views/AsyncCityView.vue
@@ -0,0 +1,28 @@
+<template>
+	<div></div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const getWeatherData = async () => {
+	try {
+		const weatherData = await axios.get(
+			`https://api.openweathermap.org/data/2.5/weather?lat=${
+				route.query.lat
+			}&lon=${route.query.lon}&exclude=minutely,hourly,alerts&appid=${
+				import.meta.env.VITE_APP_OPENWEATHER_API_KEY
+			}&units=metric`
+		);
+
+		return weatherData;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const weatherData = await getWeatherData();
+console.log(weatherData);
+</script>
