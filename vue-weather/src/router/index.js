7bf7461d1d4a5f07cd73c8746cdc7c91fc7c506f
Emre Karayaman <karayaman.emre@outlook.com>
2023-10-23 00:47:31 +0300

    Update index.js

--- vue-weather/src/router/index.js
+++ vue-weather/src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
 			component: HomeView,
 		},
 		{
-			path: "/weather/:city/:state/:country",
+			path: "/weather/:city/:country",
 			name: "city",
 			component: CityView,
 		},
